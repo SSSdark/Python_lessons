9e77f15865fea4fe5c67071e46271003f79a34a7
Shokhrukh <95573518+SSSdark@users.noreply.github.com>
2021-12-06 06:06:32 +0500

    Create 3-dars.py

---
+++ 3-dars.py
@@ -0,0 +1,16 @@
+# print ("5 ning 4-darajasi", 5**4, "ga teng")
+# print ("22 ni 4 ga bo\'lganda", 22%4, "qoldiq qoladi")
+
+# tomon = float(input("kvadratning tomonini kiriting \n>>>"))
+# S = tomon * tomon
+# P = 4 * tomon
+# print ("kvadratning yuzi", S, "ga teng")
+# print ("kvadratnign peremetri", P, 'ga teng')
+
+diametr = float(input('doiraning diametrini kiriting \n>>>'))
+r = diametr / 2
+pi = 3.14
+S = pi * r * r
+print('doiraning yuzi', S, "ga teng")
+
+print ("katetlari 6 va 7 ga teng bo'lgan to'g'ri burchakili uchburchak gipotenuzasi >>>", (((6*6)+(7*7))**0.5))
