68abdb3d9547defe6846f6f2e2579a85f0339e8a
Shokhrukh <95573518+SSSdark@users.noreply.github.com>
2021-12-07 21:18:05 +0500

    Create 6-dars.py

---
+++ 6-dars.py
@@ -0,0 +1,16 @@
+# 1-mashq
+son = float(input("istalgan sonni kiriting\n>>>"))
+print("ushbu sonning kvadrati", son ** 2, "ga teng")
+print("ushbu sonning kubi", son ** 3, "ga teng")
+
+# 2-mashq
+yosh = int(input("yoshingizni kiriting\n>>>"))
+print ("siz", 2021-yosh, "-yilda tug'ilgansiz")
+
+# 3-mashq
+son1 = float(input("1-sonni kiriting\n>>>"))
+son2 = float(input("2-sonni kiriting\n>>>"))
+print("ushbu ikki sonning yig'indisi", son1 + son2, "ga teng")
+print("ushbu ikki sonning ayirmasi", son1 - son2, "ga teng")
+print("ushbu ikki sonning ko'paytmasi", son1 * son2, "ga teng")
+print("birinchi sonning ikkinchi songa nisbati", son1 / son2, "ga teng")
