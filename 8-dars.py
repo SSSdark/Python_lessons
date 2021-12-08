80ff49d14f8e9c773bcdb9d2ae040330fa8a5058
Shokhrukh <95573518+SSSdark@users.noreply.github.com>
2021-12-08 10:08:16 +0500

    Create 8-dars.py

---
+++ 8-dars.py
@@ -0,0 +1,39 @@
+#8-dars
+# Sana: 08.12.20210
+# Shirinboyev Shohrux
+
+
+#davlatlar = ["Uzbekistan", "Russia", "USA", "Germany"]
+#print(davlatlar)
+#print(sorted(davlatlar))
+#print(sorted(davlatlar, reverse = True))
+
+#print(davlatlar)
+#davlatlar.reverse()
+#print(davlatlar)
+
+#davlatlar.sort()
+#print(davlatlar)
+#davlatlar.sort(reverse = True)
+#print(davlatlar)
+
+#juft_sonlar = list(range(120, 1200, 2))
+#yigindi = sum(juft_sonlar)
+#print(yigindi)
+#print(juft_sonlar[0:20])
+#min = min(juft_sonlar)
+#max = max(juft_sonlar)
+#print(min)
+#print(max)
+#print(len(juft_sonlar))
+
+taomlar = ["tuxum", "murabbo", "qahva", "non", "pechene", "kabob", "manti"]
+nonushta = taomlar[:]
+nonushta.append("kolbasa")
+nonushta.remove("kabob")
+nonushta.remove("manti")
+nonushta = tuple(nonushta)
+# nonushta[0] = "choy"
+print(taomlar)
+print(nonushta)
+
