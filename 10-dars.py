cd125b5e0ea139f7ea572f294006d10356441652
Shokhrukh <95573518+SSSdark@users.noreply.github.com>
2021-12-16 00:48:05 +0500

    Create 10-dars.py

---
+++ 10-dars.py
@@ -0,0 +1,50 @@
+# 10-dars
+# 15.12.2021
+# Shohrux Shirinboyev
+
+#cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+#for car in cars:
+#    if car == 'gm':
+#        print (car.upper())
+#    else:
+#        print (car.title())
+
+
+#cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+#for car in cars:
+#    if car != 'gm':
+#        print (car.title())
+#    else:
+#        print (car.upper())
+
+
+
+#login = str(input("Iltimos loginni kiriting\n>>>"))
+#if login == "Admin":
+#    print("Xush kelibsiz, Admin, foydalanuvchilar ro'yxatini ko'rasizmi")
+#else:
+#    print("Xush kelibsiz", login)
+
+
+#son1 = float(input("1-sonni kiriting\n>>>"))
+#son2 = float(input("2-sonni kiriting\n>>>"))
+#if son1 == son2:
+#    print('ushbu kiritilgan ikki son bir0biriga teng')
+#else:
+#    print("ushbu kiritlgan ikki son bir-biriga teng emas5")
+
+
+#son = float(input("Istalgan soningizni kiriting\n>>>"))
+#if son > 0:
+#    print('kiritilgan son musbat')
+#elif son == 0:
+#    print('kiritilgan son 0 ga teng')
+#else:
+#    print('kritilgan son manfiy')
+
+
+son = float(input("Istalgan bir sonni kiriting\n>>>"))
+if son >= 0:
+    print("Ushbu sonning kvadrat ildizi", son ** (1/2), "ga teng")
+else:
+    print("kiritilgan son manfiy")
