a7522dd6e93e35e15b6ad875f6340286d23e9c3a
Shokhrukh <95573518+SSSdark@users.noreply.github.com>
2021-12-19 18:58:20 +0500

    Create 12-dars.py

---
+++ 12-dars.py
@@ -0,0 +1,82 @@
+# 12-dars
+# xatolar bilan ishlash
+# Shohrux Shirinboyev
+# 19.12.20217
+
+
+#son = float(input("Juft son kiriting:\n"))
+#if (son%2) == 1:
+#   print("Bu son juft emas.")
+#else:
+#    print("Rahmat! Siz kiritgan juft son", son)
+
+
+
+#yosh = int(input("Yoshingiz nechida?"))
+#if yosh <=4 or yosh >= 60:
+#    narh = 0
+#elif yosh < 18:
+#    narh = 10000
+#else:
+#    narh = 20000
+#print(f"Chipta {narh} so'm")
+
+
+#x = float(input("Birinchi sonni kiriting: "))
+#y = float(input("Ikkinchi sonni kiriting: "))
+#if x == y :
+#    print(f"{x}={y}")
+#elif x < y:
+#    print(f"{x}<{y}")
+#else:
+#    print(f"{x}>{y}")
+
+
+#mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+#savat = []
+#for n in range(5):
+#    savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+
+#for mahsulot in savat:
+#    if mahsulot in mahsulotlar:
+#            print(f"Do'konimizda {mahsulot} bor")
+#    else:
+#            print(f"Do'konimizda {mahsulot} yo'q")
+
+
+#mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+#               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+#savat = []
+#for n in range(5):
+#    savat.append(input(f'Savatga {n+1}-mahsulotni qo\'shing: '))
+
+#bor_mahsulotlar = []
+#mavjud_emas = []
+#for mahsulot in savat:
+#    if mahsulot in mahsulotlar:
+#        bor_mahsulotlar.append(mahsulot)
+#    else:
+#        mavjud_emas.append(mahsulot)
+
+#if mavjud_emas:
+#    print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#    for mahsulot in mavjud_emas:
+#        print(mahsulot)
+#else:
+#    print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+
+#users = ['alisher1983','aziza','yasina' 'umar']
+
+#login = str(input("Yangi login tanlang:" ))
+
+#if login in users:
+#    print('Login band, yangi login tanlang!')
+#else:
+#    print("Xush kelibsiz!")
+
+
