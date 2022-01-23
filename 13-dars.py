337a346b3fdf355be5d00dad79a917306bfaede0
Shokhrukh <95573518+SSSdark@users.noreply.github.com>
2022-01-23 09:55:53 +0500

    Create 13-dars.py

---
+++ 13-dars.py
@@ -0,0 +1,5 @@
+# January 23, 2022
+# Shokhrukh Shirinboev
+# 13 (rd) lesson python =
+
+print("In this lesson we learned about github partfolio. And learned about how to create and upload our projects")
